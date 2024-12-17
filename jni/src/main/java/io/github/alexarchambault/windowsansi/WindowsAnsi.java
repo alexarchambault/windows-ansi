660f7568fe178f9b93627106fc63976e16063f7c
Alex Archambault <alexandre.archambault@gmail.com>
2024-12-17 22:55:00 +0100

    Add workaround for jansi 2.4.1 Windows ARM64 issue

--- jni/src/main/java/io/github/alexarchambault/windowsansi/WindowsAnsi.java
+++ jni/src/main/java/io/github/alexarchambault/windowsansi/WindowsAnsi.java
@@ -3,11 +3,24 @@ package io.github.alexarchambault.windowsansi;
 import org.fusesource.jansi.internal.Kernel32;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
 import java.util.Objects;
 
 public final class WindowsAnsi {
 
     static {
+        // Workaround while we can't benefit from https://github.com/fusesource/jansi/pull/292
+        String arch = System.getProperty("os.arch", "").toLowerCase(Locale.ROOT);
+        if (arch.equals("arm64") || arch.equals("aarch64")) {
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            URL dllUrl = cl.getResource("org/fusesource/jansi/internal/native/Windows/arm64/jansi.dll");
+            URL soUrl = cl.getResource("org/fusesource/jansi/internal/native/Windows/arm64/libjansi.so");
+            if (dllUrl == null && soUrl != null) {
+                System.setProperty("library.jansi.name", "libjansi.so");
+            }
+        }
+
         // Make https://github.com/fusesource/hawtjni/blob/c14fec00b9976ff6b84e62e483d678594a7d3832/hawtjni-runtime/src/main/java/org/fusesource/hawtjni/runtime/Library.java#L167 happy
         if (System.getProperty("com.ibm.vm.bitmode") == null)
             System.setProperty("com.ibm.vm.bitmode", "64");
